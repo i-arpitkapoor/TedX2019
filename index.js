599b6c0599af4d8dcd02bdb387e76b21c9083020
Arpit Kapoor <i-arpitkapoor>
2019-10-05 15:17:57 +0530

    add countdown

--- index.js
+++ index.js
@@ -137,25 +137,78 @@ requestAnimationFrame(update);
 //   CountDown Clock
 
 const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
 
-let countDown = new Date('Sep 30, 2020 00:00:00').getTime(),
-    x = setInterval(function() {
+// let countDown = new Date('Sep 30, 2021 00:00:00').getTime()
+// let x = setInterval(function () {
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+//     let now = new Date().getTime()
+//     let distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-      
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+//     // document.getElementById('days').innerText = Math.floor(distance / (day)),
+//     // document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+//     // document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+//     // document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+//     document.getElementById('days').innerText = Math.floor(distance / (day))
+//     document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour))
+//     document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute))
+//     document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second)
+//     console.log("i am here")
 
-    }, second)
+//     //do something later when date is reached
+//     //if (distance < 0) {
+//     //  clearInterval(x);
+//     //  'IT'S MY BIRTHDAY!;
+//     //}
+
+// }, second)
+
+
+// // x();
+
+
+//arpit countdown js
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
+
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = ('0' + t.days).slice(-2);
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+// var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+var deadline = new Date('Oct 12, 2019 00:00:00')//.getTime()
+initializeClock('clockdiv', deadline);
